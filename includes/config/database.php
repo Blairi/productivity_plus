d159ff0e8ee20cea5e24831892dc8625abc3cbbe
Blairi <axel.avi.fer@gmail.com>
2022-01-25 15:55:32 -0600

    solving special character bug

--- includes/config/database.php
+++ includes/config/database.php
@@ -4,6 +4,7 @@
 
 function conectarDB() : mysqli {
 	$db = mysqli_connect('localhost', 'root', '', 'productivity_plus');
+	$db->set_charset('utf8');
 
 	if(!$db){
 		echo "Error no se pudo conectar";
